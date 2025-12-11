41918aaf9234d34c6199abf9d43e89df8e4418fa
Vitaly D. <netmin@pm.me>
2025-12-11 09:09:06 +0300

    feat: add new icon with shutter design

--- wxt.config.ts
+++ wxt.config.ts
@@ -7,5 +7,16 @@ export default defineConfig({
     description: 'Focus better by turning distracting sites grayscale',
     permissions: ['storage', 'activeTab'],
     host_permissions: ['<all_urls>'],
+    icons: {
+      16: 'icons/icon-16.png',
+      48: 'icons/icon-48.png',
+      128: 'icons/icon-128.png',
+    },
+    action: {
+      default_icon: {
+        16: 'icons/icon-16.png',
+        48: 'icons/icon-48.png',
+      },
+    },
   },
 });
